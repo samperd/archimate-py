3da8cd19c782709388ed8453aba0fd8349783f95
Dave Sampson <samper.d@gmail.com>
2019-06-18 22:09:31 -0400

    updated XSL to fix param var

--- scripts/run-legislation.sh
+++ scripts/run-legislation.sh
@@ -8,11 +8,15 @@ OutDir="../model/$Layer/"
 FileName="${Element}_${UUID}.xml"
 
 # read resources file
+git add run-legislation.#!/bin/sh
+git commit -m "Updated run-legislation"
 # Pull new changes from the repository
 git pull
 # Run XSLT
 # saxon-xslt INPUTFILE XSLTFILE parametername=parametervalue > OUTPUTFILE
-saxon-xslt ../data/input/acts-and-regs/Legis.xml ../xslt/acts-and-regs/legis2archimate.xsl uuid=${UUID} outdir=${OutDir} filename=${FileName} > ../model/${Layer}/${FileName}
+# saxon-xslt ../data/input/acts-and-regs/Legis.xml ../xslt/acts-and-regs/legis2archimate.xsl uuid=${UUID} outdir=${OutDir} filename=${FileName} > ../model/${Layer}/${FileName}
+saxon-xslt -v ../data/input/acts-and-regs/Legis.xml ../xslt/acts-and-regs/legis2archimate.xsl uuid=${UUID} outdir=${OutDir} filename=${FileName}
+
 # Add file to git
 git add ../model/$Layer/${Element}_${UUID}.xml
 # commit changes to git
