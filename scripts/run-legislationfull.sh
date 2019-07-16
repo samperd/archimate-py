71cad7d056b315e49a1603b78d2589c176875e13
Dave Sampson <samper.d@gmail.com>
2019-07-15 20:51:58 -0400

    updated scripts/run-legislationfull.sh

--- scripts/run-legislationfull.sh
+++ scripts/run-legislationfull.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-URL="file:///home/sampson/development/archimate-py/data/input/acts-and-regs-bulk/Consolidation_2.0.0/English/Statutes/A-1.3.xml"
-UUID="eb0dee21-9123-4d0d-b11d-0763fa1fb403"
+UUID="$(uuidgen)"
 Layer="motivation"
 Element="Driver"
 Message="added/updated $Element Element in $Layer Layer. Updated/added element: $Element'_'$UUID.xml"
-OutDir="../model/$Layer/"
+OutDir="../data/output/model/$Layer"
+#OutDir="../model/$Layer/"
 FileName="${Element}_${UUID}.xml"
 
 # read resources file
@@ -16,12 +16,12 @@ FileName="${Element}_${UUID}.xml"
 # Run XSLT
 # saxon-xslt INPUTFILE XSLTFILE parametername=parametervalue > OUTPUTFILE
 # saxon-xslt ../data/input/acts-and-regs/Legis.xml ../xslt/acts-and-regs/legis2archimate.xsl uuid=${UUID} outdir=${OutDir} filename=${FileName} > ../model/${Layer}/${FileName}
-saxonb-xslt -ext:on $URL ../xslt/acts-and-regs/legisfull2archimate-snippet.xsl
+saxonb-xslt -ext:on ../data/input/acts-and-regs/Legis.xml ../xslt/acts-and-regs/legisfull2archi-snippet.xsl uuid=$UUID outdir=$OutDir layer=$Layer element=$Element
 
 # Add file to git
 # git add ../model/$Layer/${Element}_${UUID}.xml
 # commit changes to git
 # git commit -m "$Message"
-# Git puish
+# Git push
 # git push
 # use Archi command line to pull new model changes?
