446408edf9533f32378fa278f20091cb2bf3373a
Dave Sampson <samper.d@gmail.com>
2019-07-15 20:51:31 -0400

    updated scripts/run-legislation.sh

--- scripts/run-legislation.sh
+++ scripts/run-legislation.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-UUID="eb0dee21-9123-4d0d-b11d-0763fa1fb403"
+UUID="$(uuidgen)"
 Layer="motivation"
 Element="Driver"
 Message="added/updated $Element Element in $Layer Layer. Updated/added element: $Element'_'$UUID.xml"
-OutDir="../model/$Layer/"
+OutDir="../data/output/model/$Layer"
+#OutDir="../model/$Layer/"
 FileName="${Element}_${UUID}.xml"
 
 # read resources file
@@ -15,12 +16,12 @@ FileName="${Element}_${UUID}.xml"
 # Run XSLT
 # saxon-xslt INPUTFILE XSLTFILE parametername=parametervalue > OUTPUTFILE
 # saxon-xslt ../data/input/acts-and-regs/Legis.xml ../xslt/acts-and-regs/legis2archimate.xsl uuid=${UUID} outdir=${OutDir} filename=${FileName} > ../model/${Layer}/${FileName}
-saxonb-xslt -ext:on ../data/input/acts-and-regs/Legis.xml ../xslt/acts-and-regs/legis2archimate.xsl uuid=$UUID outdir=$OutDir
+saxonb-xslt -ext:on ../data/input/acts-and-regs/Legis.xml ../xslt/acts-and-regs/legis2archimate.xsl uuid=$UUID outdir=$OutDir layer=$Layer element=$Element
 
 # Add file to git
 # git add ../model/$Layer/${Element}_${UUID}.xml
 # commit changes to git
 # git commit -m "$Message"
-# Git puish
+# Git push
 # git push
 # use Archi command line to pull new model changes?
